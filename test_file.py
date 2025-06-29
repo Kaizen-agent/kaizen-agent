f956649943aa82bd9f81ac9dd2153bf2a3bff6d5
Yuto Suzuki <mokkumokku99@gmail.com>
2025-06-29 10:21:01 -0600

    add a deadly simple example to README and replaced marker with config-only setup

--- test_file.py
+++
@@ -1,7 +0,0 @@
-def test_function(input_data):
-    """Simple test function that returns the input data."""
-    return input_data
-
-def another_function():
-    """Another function for testing."""
-    return "hello world" 
